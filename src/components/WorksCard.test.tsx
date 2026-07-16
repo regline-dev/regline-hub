9a26bf728fd7c04eb7c11cf57172b4e60e43a3b2
regline <regline@naver.com>
2026-07-17 00:19:21 +0900

    11

--- src/components/WorksCard.test.tsx
+++ src/components/WorksCard.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
 import { WorksCard } from './WorksCard'
 import type { WorksProject } from '../data/worksProjects'
